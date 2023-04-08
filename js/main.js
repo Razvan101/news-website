083e238b76f1a4f8231ebbf1492751b772460e7a
Razvan Raduceanu <113583769+Razvan53@users.noreply.github.com>
2023-04-08 10:33:16 +0300

    Final

---
+++ js/main.js
@@ -0,0 +1,19 @@
+if (innerWidth < 600){
+  let imgEl = document.getElementById('top-img').src = 'images/image-web-3-mobile.jpg';
+}
+let menuEl = document.getElementById('menu-icon');
+let listEl = document.getElementById('navigation-bar');
+let closeEl = document.createElement('img');
+menuEl.addEventListener('click', displayMenu);
+function displayMenu(){
+  menuEl.style.display = 'none';
+  listEl.style.display = 'block';
+  closeEl.src = 'images/icon-menu-close.svg';
+  closeEl.id = 'close-menu';
+  listEl.appendChild(closeEl);
+  document.getElementById('close-menu').onclick = function(){
+    menuEl.style.display = 'block';
+    listEl.style.display = 'none';
+  }
+  console.log(listEl);
+}
